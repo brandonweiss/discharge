e57ea5358ae0188cf23539dbb8695598784e8f5c
fragosti <francesco.agosti93@gmail.com>
2018-11-02 15:40:39 -0700

    Add ability to specify the configurationPath

--- cli.js
+++ cli.js
@@ -6,22 +6,33 @@ const KnownError = require("./lib/error")
 const initCommand = require("./lib/cli/init-command")
 const deployCommand = require("./lib/cli/deploy-command")
 const distributeCommand = require("./lib/cli/distribute-command")
-const ConfigurationPath = ".discharge.json"
 
 const cli = meow(`
   Usage
     $ discharge init
     $ discharge deploy
     $ discharge distribute
-`)
+  
+  Options
+    --configPath, -c  The path and name of the discharge config to be created or used
+`, {
+  flags: {
+    configPath: {
+      type: 'string',
+      alias: 'c',
+      default: '.discharge.json',
+    }
+  }
+})
 
 updateNotifier({ pkg: cli.pkg }).notify()
 
-let command = cli.input[0]
+const command = cli.input[0]
+const configurationPath = cli.flags.configPath
 
 switch (command) {
 case undefined:    return cli.showHelp()
-case "init":       return initCommand(ConfigurationPath).catch(KnownError.catch)
-case "deploy":     return deployCommand(ConfigurationPath).catch(KnownError.catch)
-case "distribute": return distributeCommand(ConfigurationPath).catch(KnownError.catch)
+case "init":       return initCommand(configurationPath).catch(KnownError.catch)
+case "deploy":     return deployCommand(configurationPath).catch(KnownError.catch)
+case "distribute": return distributeCommand(configurationPath).catch(KnownError.catch)
 }
